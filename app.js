0887f69e44254c0cb9c3dd9de789989bd916da7c
Amit Gupta <your@email.com>
2025-11-22 01:54:15 +0530

    Add form element to index.html and update script loading order for production

--- app.js
+++ app.js
@@ -83,3 +83,15 @@
 //   ReactDOM.render(element2.root);
 
 
+
+
+                      // React
+
+
+       <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
