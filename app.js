a7237aa784f97a3db95b14de63efde1abaf1d927
Amit Gupta <your@email.com>
2025-11-21 23:19:15 +0530

    Add dynamic heading elements to the app and update HTML structure

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const element1= document.createElement("h1");
  element1.textContent = "hellow Codders";
  element1.className = 'element';
  element1.id = 'first';
+ element1.style.fontSize = "30px";
+ element1.style.backgroundColor = "orange";
+ element1.style.color ="white";
 
+ const element2= document.createElement("h2");
+ element2.textContent = "Streack is lounched";
+ element2.className = 'element';
+ element2.id = 'first';
+ element2.style.fontSize = "30px";
+ element2.style.backgroundColor = "blue";
+ element2.style.color ="pink";
 
 
 
  document.getElementById = ('root');
  root.append(element1);
+  root.append(element2);
