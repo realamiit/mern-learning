e240ad2efb70df3c40b7b29ea984f42d06966521
Amit Gupta <your@email.com>
2025-11-21 23:08:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+   //creat heading 
+
+const element1= document.createElement("h1");
+ element1.textContent = "hellow Codders";
+ element1.className = 'element';
+ element1.id = 'first';
+
+
+
+
+ document.getElementById = ('root');
+ root.append(element1);
