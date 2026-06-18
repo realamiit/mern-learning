9313c933318db560224e3c0afe217d0f6fb7f15f
Amit Gupta <amitgupta4363@email.com>
2026-06-18 16:10:20 +0530

    Day 2 : daytapype in js

---
+++ 01-javascript/practice/datatypes.js
@@ -0,0 +1,34 @@
+// ====DATA TYPE IN JAVASCRIPT =====
+
+// 1. STRING : Text Data hota hai - quots (" ") me likhte hai 
+const questionName  = "Two Sum";
+const topic = "Array";
+const difficulty = "Easy";
+
+// 2. NUMBER : intiger and decimal dono eek hi me store hoge 
+const totalQuestions = 150;
+const accuracy = 85.5;
+
+// 3. BOOLEAN : sirf 2 value true/false
+const isSolved = true;
+const isBookMarked = false;
+
+// 4. NULL : intentionally empty rakha hai — developer ne khud set kiya
+const assignedData = null;
+
+// 5. UNDEFINED : value abhi tak assign hi nahi hui
+// const lastRevisionDate; // it gave error because of const constant hota hai aur hme iski value define krni hi hogi thats why
+let lastRevisionDate;
+
+
+// 6. typeof : variable ka type check karta hai
+console.log(typeof questionName);
+console.log(typeof topic);
+console.log(typeof difficulty);
+console.log(typeof totalQuestions);
+console.log(typeof accuracy);
+console.log(typeof isSolved);
+console.log(typeof isBookMarked);
+console.log(typeof assignedData);
+console.log(typeof lastRevisionDate)
+
