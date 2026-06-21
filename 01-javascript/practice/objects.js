b89dd274c1a7aebcc8ee06adc9a413a2c80dd4d0
Amit Gupta <amitgupta4263@gmail.com>
2026-06-21 14:39:52 +0530

    Day 5: Objects basics, dot notation, key naming importance

---
+++ 01-javascript/practice/objects.js
@@ -0,0 +1,17 @@
+// =========OBJECT IN JAVASCRIPT=========
+
+// eek question DSA Tracker ke liye
+const question1 = {
+    name: "Two Sum",
+    topic: "Array",
+    difficulty: "Easy",
+    solvedDate: "2026-06-20",
+    nextRevisionDate: "2026-06-23"
+}
+
+// Dot Natation se access karna we access  this because this is impoortant to know easily the question is 
+console.log(question1.name);
+console.log(question1.difficulty);
+console.log(question1.topic);
+console.log(question1.solvedDate);
+console.log(question1.nextRevisionDate);
