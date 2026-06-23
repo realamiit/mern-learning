87e53c0413c0e8f0b919cba3dbdcef182b2e598e
Amit Gupta <amitgupta4263@gmail.com>
2026-06-23 19:04:25 +0530

    Day 7: Node.js http module, server basics, request-response cycle task add kiya

--- 02-nodejs/practice/server.js
+++ 02-nodejs/practice/server.js
@@ -17,11 +17,27 @@ const server = http.createServer((req, res) => {
 
   // req.url mein wo EXACT PATH hota hai jo browser ne request kiya
    console.log("Request aayi is Url par: ",req.url);   
+
+   if(req.url === "/dashboard"){
+    //console.log(" ")
+   }else{
+    //
+   }
    
   // res.end() response ko complete karta hai aur data bhej deta hai
  // jab tak end() nahi bolte, browser wait karta rahega response ke liye
   res.end("Hello i am Amit from DSA Tracker Backend!");
 });
+
+// ========task=========
+// const server = http.createServer/dashboard((req,res) => {
+//   res.end("Yeh dashboard page hai!");
+// })
+
+// const server = http.createServer/questions((req,res) => {
+//   res.end("Yeh home page hai!");
+// })
+
 
 // server ko ek PORT par "listen" karne ke liye bolte hain
 // port = ek number jo identify karta hai ki kis "channel" pe server sun raha hai
