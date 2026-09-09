c41392deda1dbf60177d1dc9b607dd870036ddab
Amit Gupta <amitgupta4263@gmail.com>
2026-09-09 23:14:07 +0530

    Learn Some Components

---
+++ 03-ReactJS/01-filder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>hello1</h1>
+      <h2 id='child2'>hello2</h2>
+    </div>
+    <div id="chacha">
+      <h1> chacha boy1</h1>
+      <h2>chacha boy2</h2>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 03-ReactJS/02-Components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='card'>
+      <h1>Amit Gupta</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, fuga.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
